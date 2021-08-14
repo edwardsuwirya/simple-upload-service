221ddb4b3a3e0b46cccfea0092d727c12fb12ac8
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-08-14 11:22:25 +0700

    Activity Result API implementation and Media Path content provider now can read public download or public document

--- app/src/main/java/com/enigmacamp/mysimpleupload/presentation/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/mysimpleupload/presentation/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.enigmacamp.mysimpleupload.presentation
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.enigmacamp.mysimpleupload.R
 import com.enigmacamp.mysimpleupload.data.api.RetrofitInstance
 import com.enigmacamp.mysimpleupload.data.repository.PostRepository
 import com.enigmacamp.mysimpleupload.databinding.ActivityMainBinding
@@ -21,20 +16,63 @@ import java.io.File
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainActivityViewModel
+
+    var idCardImagePath: File? = null
+    var selfieImagePath: File? = null
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) Log.i(TAG, "onRequestPermissionsResult: Granted")
+            else Log.i(TAG, "onRequestPermissionsResult: Denied")
+        }
+
+    private val openImageGalleryForIdCard =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri ->
+            Log.d(TAG, "onActivityResult: $imageUri")
+            binding.idCardImageView.setImageURI(imageUri)
+            imageUri?.let {
+                val file = MediaPath.getRealPathFromURI(contentResolver, imageUri)
+                file?.let {
+                    idCardImagePath = File(it)
+                }
+            }
+        }
+    private val openImageGalleryForSelfie =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri ->
+            Log.d(TAG, "onActivityResult: $imageUri")
+            binding.selfieImageView.setImageURI(imageUri)
+            imageUri?.let {
+                val file = MediaPath.getRealPathFromURI(contentResolver, imageUri)
+                file?.let {
+                    selfieImagePath = File(it)
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initViewModel()
 
+        setupPermission()
+
         binding.apply {
+            pickIdCardButton.setOnClickListener {
+                openImageGalleryForIdCard.launch("image/*")
+            }
+            pickSelfieButton.setOnClickListener {
+                openImageGalleryForSelfie.launch("image/*")
+            }
+
             uploadButton.setOnClickListener {
-                setupPermission()
-                val gallery = Intent(Intent.ACTION_PICK)
-                gallery.type = "image/*"
-                startActivityForResult(gallery, REQUEST_CODE)
+//                viewModel.upload()
             }
         }
+    }
+
+    private fun setupPermission() {
+        requestPermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
     }
 
     private fun initViewModel() {
@@ -47,55 +85,7 @@ class MainActivity : AppCompatActivity() {
         }).get(MainActivityViewModel::class.java)
     }
 
-    private fun setupPermission() {
-        val permission =
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            )
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                REQUEST_CODE
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            REQUEST_CODE -> {
-                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Log.i(TAG, "onRequestPermissionsResult: Denied")
-                } else {
-                    Log.i(TAG, "onRequestPermissionsResult: Granted")
-                }
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE) {
-            val imageUri = data?.data
-            Log.d(TAG, "onActivityResult: $imageUri")
-            binding.imageView.setImageURI(imageUri)
-            imageUri?.let {
-                val file = MediaPath.getRealPathFromURI(contentResolver, imageUri)
-                file?.let {
-                    viewModel.upload(File(it), 1)
-                }
-            }
-        }
-    }
-
     companion object {
-        const val REQUEST_CODE = 1000
         const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/com/enigmacamp/mysimpleupload/utils/MediaPath.kt
+++ app/src/main/java/com/enigmacamp/mysimpleupload/utils/MediaPath.kt
@@ -1,20 +1,56 @@
 package com.enigmacamp.mysimpleupload.utils
 
 import android.content.ContentResolver
+import android.content.ContentUris
+import android.database.Cursor
 import android.net.Uri
+import android.provider.DocumentsContract
 import android.provider.MediaStore
+import android.util.Log
 
 object MediaPath {
     fun getRealPathFromURI(contentResolver: ContentResolver, contentUri: Uri): String? {
-        val cursor = contentResolver.query(contentUri, null, null, null, null)
+        Log.d("MainActivity", "getUriAuthority: ${contentUri.authority}")
+        var cursor: Cursor? = null
+        if ("com.android.providers.media.documents".equals("${contentUri.authority}")) {
+            cursor = contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+            )
+        } else if ("com.android.providers.downloads.documents".equals("${contentUri.authority}")) {
+            val id = DocumentsContract.getDocumentId(contentUri)
+            if (id.startsWith("raw:")) {
+                return id.replaceFirst("raw:", "");
+            }
+            val downloadUri = ContentUris.withAppendedId(
+                Uri.parse("content://downloads/public_downloads"), id.toLong()
+            )
+            cursor = contentResolver.query(
+                downloadUri,
+                null,
+                null,
+                null,
+                null
+            )
+
+        } else {
+            cursor = contentResolver.query(contentUri, null, null, null, null)
+        }
+
         return if (cursor == null) {
             contentUri.path
         } else {
             cursor.moveToFirst()
             val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+            Log.d("MainActivity", "getRealPathFromURI: $idx")
             val path = cursor.getString(idx)
             cursor.close()
             path
         }
     }
 }
+//content://com.android.providers.media.documents/document/image%3A81 => error
+//content://com.google.android.apps.photos.contentprovider/-1/1/content%3A%2F%2Fmedia%2Fexternal%2Fimages%2Fmedia%2F80/ORIGINAL/NONE/1194803451
